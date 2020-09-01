85cf3ec33126ddd428c47d7d87e1372bd60b8462
Dominic A. Miralles <60880034+EngrDevDom@users.noreply.github.com>
2020-09-01 22:15:39 +0800

    first commit

---
+++ Electro_Book.py
@@ -0,0 +1,20 @@
+""" ELECTRO_BOOK """
+
+# PART 1 :  Lessons/Theory
+def Lessons_Theory:
+    print('Chapter 1 : ELECTRICITY AND MAGNETISM FUNDAMENTALS')
+    print('-> Quantities and Units')
+    print('-> Atomic Structure')
+    print('-> Electrical Quantities')
+
+# PART 2 : Questions/Answers
+def Questions_Answers:
+    print('Practice Questions')
+
+# PART 3 : Status/Discussion
+def Status_Discussion:
+    print('Status:')
+    print('Correct Answers:')
+    print('Wrong Answers:')
+
+ 
---
+++ README.txt
@@ -0,0 +1,9 @@
+ELECTRO_BOOK
+ - a new model of a book made electronically, created for the 
+   new normal type of studying for students.
+ - This can serve as a reference/reviewer for learning
+   simplified as much as possible.
+
+PART 1 : Lessons/Theory
+PART 2 : Questions/Answers
+PART 3 : Status/Discussion
